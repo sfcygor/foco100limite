226b87890245544a89c33bdab91307311f25263b
Foco User <usuario@foco100limite.com>
2026-05-29 01:40:13 -0400

    fix: autocrop logo to make it larger in favicon

--- resize.js
+++ resize.js
@@ -5,16 +5,19 @@ async function generateFavicons() {
     console.log('Loading logo.png...');
     const image = await Jimp.read('./public/logo.png');
 
+    console.log('Autocropping transparent padding...');
+    image.autocrop();
+
     console.log('Generating 16x16...');
-    const img16 = image.clone().resize(16, 16);
+    const img16 = image.clone().contain(16, 16, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
     await img16.writeAsync('./public/favicon-16x16.png');
 
     console.log('Generating 32x32...');
-    const img32 = image.clone().resize(32, 32);
+    const img32 = image.clone().contain(32, 32, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
     await img32.writeAsync('./public/favicon-32x32.png');
 
     console.log('Generating apple-touch-icon...');
-    const img180 = image.clone().resize(180, 180);
+    const img180 = image.clone().contain(180, 180, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
     await img180.writeAsync('./public/apple-touch-icon.png');
     
     console.log('Generating favicon.ico...');
